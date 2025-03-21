73c35748294d66fffffc8bfd88cdcd1e8b4c72ac
Riccardo Cappuzzo <riccardo.cappuzzo@gmail.com>
2025-03-21 09:59:26 +0100

    Built site for gh-pages

--- slides/expressions/load_data.py
+++
@@ -1,9 +0,0 @@
-import skrub
-from skrub import selectors as s
-import skrub.datasets
-
-data = skrub.datasets.fetch_credit_fraud()
-
-X = skrub.X(data.baskets[["ID"]])
-y = skrub.y(data.baskets["fraud_flag"])
-products = skrub.var("products", data.products)
